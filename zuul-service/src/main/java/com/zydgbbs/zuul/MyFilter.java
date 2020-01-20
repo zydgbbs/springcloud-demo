a01f928a796a10926bc5de61da77a0c0b81d95c5
zydgbbs <admin@zydgbbs.com>
2020-01-20 22:24:24 +0800

    filter

---
+++ zuul-service/src/main/java/com/zydgbbs/zuul/MyFilter.java
@@ -0,0 +1,44 @@
+package com.zydgbbs.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter{
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if(accessToken != null){
+            return null;
+        }
+        ctx.setSendZuulResponse(false);
+        ctx.setResponseStatusCode(401);
+        try{
+            ctx.getResponse().getWriter().write("token is empty");
+        }catch (Exception e){
+
+        }
+        return null;
+    }
+}
